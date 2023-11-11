76bf51d615865f17dbafc2d08d048b1ce0142d10
Marcos Prata <mhpjunior3@gmail.com>
2023-11-11 13:36:42 -0300

    remove unnecessary comments

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.lanchonete_db.address
